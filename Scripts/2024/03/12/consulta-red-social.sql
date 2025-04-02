a9d4fd1751d7b27cb069fd782bb91dd6a6a64906
Ruiz Elizalde Misael <L23170055@culiacan.tecnm.mx>
2025-04-02 17:52:27 +0000

    clase 02-03 abril

--- Scripts/2024/03/12/consulta-red-social.sql
+++ Scripts/2024/03/12/consulta-red-social.sql
@@ -37,5 +37,36 @@ select *
 --consulta hecha solo
 select h1.name, h2.name from Highschooler h1
 join Likes l on h1.ID = l.ID1
- join Highschooler h2 on h2.ID = l.ID2
-    
+join Likes l2 on h1.ID = l.ID2
+join Highschooler h2 on l2.ID1 = l.ID1
+    where l.ID2 = l2.ID1
+
+--consultas estudiantes que se gustan mutuamente
+select * from likes l1 
+    join likes l2 on l1.ID2 = l2.ID1
+    where l1.id1 = l2.ID2
+
+-- consultas con los nombres *profesor*
+select * from likes l1 
+    join likes l2 on l1.ID2 = l2.ID1
+    join Highschooler h on h.id = l1.ID1
+    join Highschooler h2 on h2.ID = l2.ID2
+    where l1.id1 = l2.ID2
+        and l1.id1 < l1.id2
+--consulta hecha solo 
+select *
+    from Highschooler h1
+    join likes l on h1.ID = l.ID1
+    join Highschooler h2 on h2.ID = l.ID2
+    join likes l2 on l.ID2 = l2.ID1
+    where l.id1 = l2.ID2
+
+select h1.name, h2.name, h1.grade, h2.grade
+    from Highschooler h1
+    join likes l on h1.ID = l.ID1
+    join Highschooler h2 on h2.ID = l.ID2
+    join likes l2 on l.ID2 = l2.ID1
+    where l.id1 = l2.ID2 
+        and l.id1 < l.id2
+
+--4 situacion{ triangulo amoroso}
